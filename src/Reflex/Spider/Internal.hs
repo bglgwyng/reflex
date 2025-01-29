b903cdf7ba91afb810c8dd4ca07693c721e01b02
bgl gwyng <bgl@gwyng.com>
2025-01-29 18:00:39 +0900

    Test update.

--- src/Reflex/Spider/Internal.hs
+++ src/Reflex/Spider/Internal.hs
@@ -2259,7 +2259,7 @@ mergeIntCheap d = Event $ \sub -> do
               liftIO $ for_ oldParents $ \oldParent -> do
                 oldParentHeight <- getEventSubscribedHeight $ _eventSubscription_subscribed oldParent
 
-                print ("updateMe", oldParentHeight)
+                print ("updateMe2", oldParentHeight)
                 modifyIORef' heightBagRef $ heightBagRemove oldParentHeight
               return $ IntMap.elems oldParents
     let changeSubscriber = Subscriber
